b39603b7621b1bcf6f3498b4570e0d801edd538d
meileur <108830089+meileur@users.noreply.github.com>
2020-06-25 21:37:05 +0000

    chore(package): update rollup to version 0.46.0

--- Code/Chenglong/feature_combiner.py
+++ Code/Chenglong/feature_combiner.py
@@ -265,4 +265,193 @@ class StackingCombiner:
         if self.meta_feature_dict:
             cb = Combiner(feature_dict=self.meta_feature_dict, 
                         feature_name=self.feature_name, 
-                        feature_suffix=".pkl
+                        feature_suffix=".pkl", 
+                        corr_threshold=self.corr_threshold)
+            cb.combine()
+            self.X_train_basic = cb.X_train
+            self.X_test_basic = cb.X_test
+            self.feature_names_basic = cb.feature_names_basic
+            self.feature_names.extend(cb.feature_names)
+        else:
+            self.X_train_basic = None
+            self.X_test_basic = None
+
+        # combine other features
+        dfAll = pkl_utils._load(config.INFO_DATA)
+
+        dfTrain = dfAll.iloc[:TRAIN_SIZE].copy()
+
+        dfTest = dfAll.iloc[TRAIN_SIZE:].copy()
+        self.id_test = dfTest["id"].values.astype(int)
+
+        ## all
+        first = True
+        feat_cnt = 0
+        feature_dir = "%s/All" % (config.OUTPUT_DIR)
+        for file_name in sorted(os.listdir(feature_dir)):
+            if self.feature_suffix in file_name:
+                ## NOTE the name is valid.pred.MODEL.csv and test.pred.MODEL.csv
+                fname = file_name.split(".")[2]
+                if fname not in self.feature_list:
+                    continue
+                if first:
+                    self.logger.info("Run for all...")
+                    first = False
+                # load prediction
+                x = self.load_feature(feature_dir, "test.pred."+fname)
+                x = np.nan_to_num(x)
+                dim = np_utils._dim(x)
+                dfTest[fname] = x
+                feat_cnt += 1
+                self.feature_names_cv.append(fname)
+                self.feature_names.append(fname)
+                self.logger.info("Combine {:>3}/{:>3} feat: {} ({}D)".format(
+                    feat_cnt, len(self.feature_list), fname, dim))
+                # load probability if any
+                try:
+                    x = self.load_feature(feature_dir, "test.proba."+fname, 
+                                        columns=None, columns_pattern="proba")
+                    x = np.nan_to_num(x)
+                    dim = np_utils._dim(x)
+                    for i in range(dim):
+                        dfTest["%s_proba%d"%(fname, i)] = x[:,i]
+                    self.logger.info("Combine {:>3}/{:>3} proba feat: {} ({}D)".format(
+                        feat_cnt, len(self.feature_list), fname, dim))
+                    self.feature_names.extend(["%s_proba%d"%(fname, i) for i in range(dim)])
+                except:
+                    pass
+
+        dfTest.fillna(config.MISSING_VALUE_NUMERIC, inplace=True)
+        self.X_test = dfTest.drop(["id","relevance"], axis=1).values.astype(float)
+        if self.meta_feature_dict:
+            self.X_test = np.hstack([self.X_test_basic, self.X_test])
+
+        ## for cv features
+        first = True
+        for run in range(1,self.n_iter+1):
+            feat_cnt = 0
+            idx1 = splitter_level1[run-1][1]
+            idx2 = splitter_level2[run-1][1]
+            if self.feature_level == 2:
+                idx = idx1
+            elif self.feature_level == 3:
+                idx = [ idx1[i] for i in idx2 ]
+            self.splitter_prev[run-1] = idx
+            dfTrain_cv = dfTrain.iloc[idx].copy()
+            feature_dir = "%s/Run%d" % (config.OUTPUT_DIR, run)
+            for file_name in sorted(os.listdir(feature_dir)):
+                if self.feature_suffix in file_name:
+                    ## NOTE the name is valid.pred.MODEL.csv and test.pred.MODEL.csv
+                    fname = file_name.split(".")[2]
+                    if (fname not in self.feature_list) or (fname not in self.feature_names_cv):
+                        continue
+                    if first:
+                        self.logger.info("Run for cv...")
+                        first = False
+                    if feat_cnt == 0:
+                        self.logger.info("Run %d"%run)
+                    # load prediction
+                    x = self.load_feature(feature_dir, "valid.pred."+fname)
+                    x = np.nan_to_num(x)
+                    dim = np_utils._dim(x)
+                    # also including level 1 models' preditions
+                    if x.shape[0] > len(idx):
+                        x = x[idx2]
+                    dfTrain_cv[fname] = x
+                    feat_cnt += 1
+                    self.logger.info("Combine {:>3}/{:>3} feat: {} ({}D)".format(
+                        feat_cnt, len(self.feature_list), fname, dim))
+                    # load probability if any
+                    try:
+                        x = self.load_feature(feature_dir, "valid.proba."+fname, 
+                                            columns=None, columns_pattern="proba")
+                        x = np.nan_to_num(x)
+                        dim = np_utils._dim(x)
+                        # also including level 1 models' preditions
+                        if x.shape[0] > len(idx):
+                            x = x[idx2]
+                        for i in range(dim):
+                            dfTrain_cv["%s_proba%d"%(fname, i)] = x[:,i]
+                        self.logger.info("Combine {:>3}/{:>3} proba feat: {} ({}D)".format(
+                            feat_cnt, len(self.feature_list), fname, dim))
+                    except:
+                        pass
+
+            dfTrain_cv.fillna(config.MISSING_VALUE_NUMERIC, inplace=True)
+            if run == 1:
+                self.y_train_cv = [0]*self.n_iter
+                self.X_train_cv = [0]*self.n_iter
+            self.y_train_cv[run-1] = dfTrain_cv["relevance"].values.astype(float)
+            self.X_train_cv[run-1] = dfTrain_cv.drop(["id","relevance"], axis=1).values.astype(float)
+
+        if self.has_basic:
+            self.logger.info("Overall Shape: %d x %d"%(
+                len(self.y_train_cv[self.n_iter-1]), self.X_train_basic.shape[1] + self.X_train_cv[self.n_iter-1].shape[1]))
+        else:
+            self.logger.info("Overall Shape: %d x %d"%(
+                len(self.y_train_cv[self.n_iter-1]), self.X_train_cv[self.n_iter-1].shape[1]))
+        self.logger.info("Done combinning.")
+        
+        return self
+
+    def save(self):
+        data_dict = {
+            "X_train_basic": self.X_train_basic,
+            "y_train_cv": self.y_train_cv,
+            "X_train_cv": self.X_train_cv,
+            "X_test": self.X_test,                    
+            "id_test": self.id_test,
+            "splitter_prev": self.splitter_prev,
+            "splitter": self.splitter,
+            "n_iter": self.n_iter,
+            "has_basic": self.has_basic,
+            "feature_names": self.feature_names,
+        }
+        fname = os.path.join(config.FEAT_DIR+"/Combine", self.feature_name+config.FEAT_FILE_SUFFIX)
+        pkl_utils._save(fname, data_dict)
+        self.logger.info("Save to %s" % fname)
+
+
+#------------------------- Main ------------------------------------
+def parse_args(parser):
+    parser.add_option("-l", "--level", default=1, type="int", 
+        dest="feature_level", help="feature level, e.g., 1, 2, 3")
+    parser.add_option("-c", "--config", default="feature_conf", type="string", 
+        dest="feature_conf", help="feature config name")
+    parser.add_option("-n", "--name", default="basic%s"%time_utils._timestamp(), 
+        type="string", dest="feature_name", help="feature name")
+    parser.add_option("-s", "--suffix", default=".pkl", type="string", 
+        dest="feature_suffix", help="feature suffix")
+    parser.add_option("-m", "--meta_config", default="feature_conf_meta", 
+        type="string", dest="feature_conf_meta", help="meta feature config name")
+    parser.add_option("-t", "--threshold", default=0.0, type="float", 
+        dest="corr_threshold", help="correlation threshold for dropping features")
+    (options, args) = parser.parse_args()
+    return options, args
+
+
+def main(options):
+    feature_conf = imp.load_source("", 
+        os.path.join(config.FEAT_CONF_DIR, options.feature_conf+".py"))
+    if options.feature_level == 1:
+        combiner = Combiner(feature_dict=feature_conf.feature_dict,
+                            feature_name=options.feature_name,
+                            feature_suffix=options.feature_suffix,
+                            corr_threshold=options.corr_threshold)
+    elif options.feature_level > 1:
+        feature_conf_meta = imp.load_source("", 
+            os.path.join(config.FEAT_CONF_DIR, options.feature_conf_meta+".py"))
+        combiner = StackingCombiner(feature_list=feature_conf.feature_list,
+                                    feature_name=options.feature_name,
+                                    feature_suffix=options.feature_suffix,
+                                    feature_level=options.feature_level,
+                                    meta_feature_dict=feature_conf_meta.feature_dict,
+                                    corr_threshold=options.corr_threshold)
+
+    combiner.combine()
+    combiner.save()
+
+
+if __name__ == "__main__":
+    parser = OptionParser()
+    options, args = parse
