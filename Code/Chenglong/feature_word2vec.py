b622b99448603c7e50cb9d1719034a35e8cdbc7d
meileur <108830089+meileur@users.noreply.github.com>
2020-06-28 19:17:38 +0000

    excel

--- Code/Chenglong/feature_word2vec.py
+++ Code/Chenglong/feature_word2vec.py
@@ -175,4 +175,103 @@ class Word2Vec_CosineSim(Word2Vec_BaseEstimator):
         for m1 in self.aggregation_mode_prev:
             for m in self.aggregation_mode:
                 n = "Word2Vec_%s_D%d_CosineSim_%s_%s"%(
-                    self.model_prefix, self
+                    self.model_prefix, self.vector_size, string.capwords(m1), string.capwords(m))
+                feat_name.append(n)
+        return feat_name
+    
+    def transform_one(self, obs, target, id):
+        val_list = []
+        obs_tokens = nlp_utils._tokenize(obs, token_pattern)
+        target_tokens = nlp_utils._tokenize(target, token_pattern)
+        for obs_token in obs_tokens:
+            _val_list = []
+            if obs_token in self.model:
+                for target_token in target_tokens:
+                    if target_token in self.model:
+                        sim = dist_utils._cosine_sim(self.model[obs_token], self.model[target_token]) 
+                        _val_list.append(sim)
+            if len(_val_list) == 0:
+                _val_list = [config.MISSING_VALUE_NUMERIC]
+            val_list.append( _val_list )
+        if len(val_list) == 0:
+            val_list = [[config.MISSING_VALUE_NUMERIC]]
+        return val_list
+
+
+# ---------------------------- Main --------------------------------------
+def main(which):
+    logname = "generate_feature_word2vec_%s_%s.log"%(which, time_utils._timestamp())
+    logger = logging_utils._get_logger(config.LOG_DIR, logname)
+    #### NOTE: use data BEFORE STEMMinG
+    dfAll = pkl_utils._load(config.ALL_DATA_LEMMATIZED)
+
+    word2vec_model_dirs = []
+    model_prefixes = []
+    if which == "homedepot":
+        ## word2vec model trained with Homedepot dataset: brand/color/query/title/description
+        word2vec_model_dirs.append( config.WORD2VEC_MODEL_DIR + "/Homedepot-word2vec-D%d-min_count%d.model"%(config.EMBEDDING_DIM, config.EMBEDDING_MIN_COUNT) )
+        model_prefixes.append( "Homedepot" )
+    elif which == "wikipedia":
+        ## word2vec model pretrained with Wikipedia+Gigaword 5
+        word2vec_model_dirs.append( config.GLOVE_WORD2VEC_MODEL_DIR + "/glove.6B.300d.txt" )
+        model_prefixes.append( "Wikipedia" )
+    elif which == "google":
+        ## word2vec model pretrained with Google News
+        word2vec_model_dirs.append( config.WORD2VEC_MODEL_DIR + "/GoogleNews-vectors-negative300.bin" )
+        model_prefixes.append( "GoogleNews" )
+
+    for word2vec_model_dir, model_prefix in zip(word2vec_model_dirs, model_prefixes):
+        ## load model
+        try:
+            if ".bin" in word2vec_model_dir:
+                word2vec_model = gensim.models.Word2Vec.load_word2vec_format(word2vec_model_dir, binary=True)
+            elif ".txt" in word2vec_model_dir:
+                word2vec_model = gensim.models.Word2Vec.load_word2vec_format(word2vec_model_dir, binary=False)
+            else:
+                word2vec_model = gensim.models.Word2Vec.load(word2vec_model_dir)
+        except:
+            continue
+
+        # ## standalone (not used in model building)
+        # obs_fields = ["search_term", "product_title", "product_description"]
+        # generator = Word2Vec_Centroid_Vector
+        # param_list = [word2vec_model, model_prefix]
+        # sf = StandaloneFeatureWrapper(generator, dfAll, obs_fields, param_list, config.FEAT_DIR, logger)
+        # sf.go()
+
+        ## pairwise
+        generators = [
+            Word2Vec_Importance,
+            Word2Vec_N_Similarity, 
+            Word2Vec_N_Similarity_Imp, 
+            Word2Vec_Centroid_RMSE, 
+            Word2Vec_Centroid_RMSE_IMP,
+            # # not used in final submission
+            # Word2Vec_Centroid_Vdiff, 
+        ]
+        obs_fields_list = []
+        target_fields_list = []
+        obs_fields_list.append( ["search_term", "search_term_alt", "search_term_auto_corrected"][:1] )
+        target_fields_list.append( ["product_title", "product_description", "product_attribute", "product_brand", "product_color"] )
+        for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+            for generator in generators:
+                param_list = [word2vec_model, model_prefix]
+                pf = PairwiseFeatureWrapper(generator, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+                pf.go()
+
+        ## cosine sim
+        generators = [
+            Word2Vec_CosineSim,
+        ]
+        # double aggregation
+        aggregation_mode_prev = ["mean", "max", "min", "median"]
+        aggregation_mode = ["mean", "std", "max", "min", "median"]
+        for obs_fields, target_fields in zip(obs_fields_list, target_fields_list):
+            for generator in generators:
+                param_list = [word2vec_model, model_prefix, aggregation_mode, aggregation_mode_prev]
+                pf = PairwiseFeatureWrapper(generator, dfAll, obs_fields, target_fields, param_list, config.FEAT_DIR, logger)
+                pf.go()
+
+
+if __name__ == "__main__":
+    main(sys.argv[1])
