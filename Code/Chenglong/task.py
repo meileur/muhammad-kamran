4afa6ef8fb0ee6d578781a89b00fa26e10f28236
meileur <108830089+meileur@users.noreply.github.com>
2020-07-08 06:32:20 +0000

    Update task.json

--- Code/Chenglong/task.py
+++ Code/Chenglong/task.py
@@ -169,4 +169,219 @@ class Feature:
 
         return X_train, y_train, X_valid, y_valid
 
- 
+    ## for refit
+    def _get_train_test_data(self):
+        # feature
+        X_basic = self.data_dict["X_train_basic"]
+        if self.data_dict["basic_only"]:
+            X_train = X_basic
+        else:
+            X_train = np.hstack((X_basic, self.data_dict["X_train_cv_all"]))
+        # test already contains cv features if any
+        # in the order [basic, cv]
+        X_test = self.data_dict["X_test"]
+        # label
+        y_train = self.data_dict["y_train"]
+
+        return X_train, y_train, X_test
+
+    ## for feature importance
+    def _get_feature_names(self):
+        return self.data_dict["feature_names"]
+
+
+class StackingFeature(Feature):
+    def __init__(self, feature_name):
+        super().__init__(feature_name)
+        self.splitter_prev = self.data_dict["splitter_prev"]
+
+    ## for CV
+    def _get_train_valid_data(self, i):
+        # feature
+        X_train_cv = self.data_dict["X_train_cv"][i][self.splitter[i][0]]
+        X_valid_cv = self.data_dict["X_train_cv"][i][self.splitter[i][1]]
+        if self.data_dict["has_basic"]:
+            X_train_basic = self.data_dict["X_train_basic"][self.splitter_prev[i]]
+            X_train = np.hstack([X_train_basic[self.splitter[i][0]], X_train_cv])
+            X_valid = np.hstack([X_train_basic[self.splitter[i][1]], X_valid_cv])
+        # label
+        y_train = self.data_dict["y_train_cv"][i][self.splitter[i][0]]
+        y_valid = self.data_dict["y_train_cv"][i][self.splitter[i][1]]
+
+        return X_train, y_train, X_valid, y_valid
+
+    ## for refit
+    def _get_train_test_data(self, i):
+        # feature
+        X_train = self.data_dict["X_train_cv"][i]
+        if self.data_dict["has_basic"]:
+            X_train_basic = self.data_dict["X_train_basic"][self.splitter_prev[i]]
+            X_train = np.hstack([X_train_basic, X_train])
+        # test already contains basic features if any
+        # in the order [basic, cv]
+        X_test = self.data_dict["X_test"]
+        # label
+        y_train = self.data_dict["y_train_cv"][i]
+
+        return X_train, y_train, X_test
+
+
+class Task:
+    def __init__(self, learner, feature, suffix, logger, verbose=True, plot_importance=False):
+        self.learner = learner
+        self.feature = feature
+        self.suffix = suffix
+        self.logger = logger
+        self.verbose = verbose
+        self.plot_importance = plot_importance
+        self.n_iter = self.feature.n_iter
+        self.rmse_cv_mean = 0
+        self.rmse_cv_std = 0
+
+    def __str__(self):
+        return "[Feat@%s]_[Learner@%s]%s"%(str(self.feature), str(self.learner), str(self.suffix))
+
+    def _print_param_dict(self, d, prefix="      ", incr_prefix="      "):
+        for k,v in sorted(d.items()):
+            if isinstance(v, dict):
+                self.logger.info("%s%s:" % (prefix,k))
+                self._print_param_dict(v, prefix+incr_prefix, incr_prefix)
+            else:
+                self.logger.info("%s%s: %s" % (prefix,k,v))
+
+    def cv(self):
+        start = time.time()
+        if self.verbose:
+            self.logger.info("="*50)
+            self.logger.info("Task")
+            self.logger.info("      %s" % str(self.__str__()))
+            self.logger.info("Param")
+            self._print_param_dict(self.learner.param_dict)
+            self.logger.info("Result")
+            self.logger.info("      Run      RMSE        Shape")
+    
+        rmse_cv = np.zeros(self.n_iter)
+        for i in range(self.n_iter):
+            # data
+            X_train, y_train, X_valid, y_valid = self.feature._get_train_valid_data(i)
+            # fit
+            self.learner.fit(X_train, y_train)
+            y_pred = self.learner.predict(X_valid)
+            rmse_cv[i] = dist_utils._rmse(y_valid, y_pred)
+            # log
+            self.logger.info("      {:>3}    {:>8}    {} x {}".format(
+                i+1, np.round(rmse_cv[i],6), X_train.shape[0], X_train.shape[1]))
+            # save
+            fname = "%s/Run%d/valid.pred.%s.csv"%(config.OUTPUT_DIR, i+1, self.__str__())
+            df = pd.DataFrame({"target": y_valid, "prediction": y_pred})
+            df.to_csv(fname, index=False, columns=["target", "prediction"])
+            if hasattr(self.learner.learner, "predict_proba"):
+                y_proba = self.learner.learner.predict_proba(X_valid)
+                fname = "%s/Run%d/valid.proba.%s.csv"%(config.OUTPUT_DIR, i+1, self.__str__())
+                columns = ["proba%d"%i for i in range(y_proba.shape[1])]
+                df = pd.DataFrame(y_proba, columns=columns)
+                df["target"] = y_valid
+                df.to_csv(fname, index=False)
+
+        self.rmse_cv_mean = np.mean(rmse_cv)
+        self.rmse_cv_std = np.std(rmse_cv)
+        end = time.time()
+        _sec = end - start
+        _min = int(_sec/60.)
+        if self.verbose:
+            self.logger.info("RMSE")
+            self.logger.info("      Mean: %.6f"%self.rmse_cv_mean)
+            self.logger.info("      Std: %.6f"%self.rmse_cv_std)
+            self.logger.info("Time")
+            if _min > 0:
+                self.logger.info("      %d mins"%_min)
+            else:
+                self.logger.info("      %d secs"%_sec)
+            self.logger.info("-"*50)
+        return self
+
+    def refit(self):
+        X_train, y_train, X_test = self.feature._get_train_test_data()
+        if self.plot_importance:
+            feature_names = self.feature._get_feature_names()
+            self.learner.fit(X_train, y_train, feature_names)
+            y_pred = self.learner.predict(X_test, feature_names)
+        else:
+            self.learner.fit(X_train, y_train)
+            y_pred = self.learner.predict(X_test)
+        
+        id_test = self.feature.data_dict["id_test"].astype(int)
+
+        # save
+        fname = "%s/%s/test.pred.%s.csv"%(config.OUTPUT_DIR, "All", self.__str__())
+        pd.DataFrame({"id": id_test, "prediction": y_pred}).to_csv(fname, index=False)
+        if hasattr(self.learner.learner, "predict_proba"):
+            if self.plot_importance:
+                feature_names = self.feature._get_feature_names()
+                y_proba = self.learner.learner.predict_proba(X_test, feature_names)
+            else:
+                y_proba = self.learner.learner.predict_proba(X_test)
+            fname = "%s/%s/test.proba.%s.csv"%(config.OUTPUT_DIR, "All", self.__str__())
+            columns = ["proba%d"%i for i in range(y_proba.shape[1])]
+            df = pd.DataFrame(y_proba, columns=columns)
+            df["id"] = id_test
+            df.to_csv(fname, index=False)
+
+        # submission
+        fname = "%s/test.pred.%s.[Mean%.6f]_[Std%.6f].csv"%(
+            config.SUBM_DIR, self.__str__(), self.rmse_cv_mean, self.rmse_cv_std)
+        pd.DataFrame({"id": id_test, "relevance": y_pred}).to_csv(fname, index=False)
+
+        # plot importance
+        if self.plot_importance:
+            ax = self.learner.plot_importance()
+            ax.figure.savefig("%s/%s.pdf"%(config.FIG_DIR, self.__str__()))
+        return self
+
+    def go(self):
+        self.cv()
+        self.refit()
+        return self
+
+
+class StackingTask(Task):
+    def __init__(self, learner, feature, suffix, logger, verbose=True, refit_once=False):
+        super().__init__(learner, feature, suffix, logger, verbose)
+        self.refit_once = refit_once
+
+    def refit(self):
+        for i in range(self.n_iter):
+            if self.refit_once and i >= 1:
+                break
+            X_train, y_train, X_test = self.feature._get_train_test_data(i)
+            self.learner.fit(X_train, y_train)
+            if i == 0:
+                y_pred = self.learner.predict(X_test)
+                if hasattr(self.learner.learner, "predict_proba"):
+                    y_proba = self.learner.learner.predict_proba(X_test)
+            else:
+                y_pred += self.learner.predict(X_test)
+                if hasattr(self.learner.learner, "predict_proba"):
+                    y_proba += self.learner.learner.predict_proba(X_test)
+        if not self.refit_once:
+            y_pred /= float(self.n_iter)
+            if hasattr(self.learner.learner, "predict_proba"):
+                y_proba /= float(self.n_iter)
+
+        id_test = self.feature.data_dict["id_test"].astype(int)
+
+        # save
+        fname = "%s/%s/test.pred.%s.csv"%(config.OUTPUT_DIR, "All", self.__str__())
+        pd.DataFrame({"id": id_test, "prediction": y_pred}).to_csv(fname, index=False)
+        if hasattr(self.learner.learner, "predict_proba"):
+            fname = "%s/%s/test.proba.%s.csv"%(config.OUTPUT_DIR, "All", self.__str__())
+            columns = ["proba%d"%i for i in range(y_proba.shape[1])]
+            print(y_proba.shape)
+            print(len(columns))
+            df = pd.DataFrame(y_proba, columns=columns)
+            df["id"] = id_test
+            df.to_csv(fname, index=False)
+
+        # submission
+        fname = "%s/test.pred.%s.[Mean%.6f]_[Std%.6f].csv"%(
+            config.SUBM_DIR, self.__str__(), self.rmse_cv_mean, self.rmse_cv_
